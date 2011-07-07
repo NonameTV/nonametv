1b81686397dd02337ea23a29f01220fdf8e21d00
Joakim Nylen <joakim@tvzon.se>
2011-07-07 15:24:58 +0200

    New importer BBC Worldwide channels

---
+++ lib/NonameTV/Importer/BBCWW.pm
@@ -0,0 +1,234 @@
+package NonameTV::Importer::BBCWW;
+
+use strict;
+use warnings;
+
+=pod
+Importer for BBC Worldwide
+
+Channels: BBC Entertainment, BBC Knowledge, BBC HD, BBC Lifestyle, CBeebies
+
+The excel files is downloaded from BBCWWChannels.com
+
+Every month is runned as a seperate batch.
+
+=cut
+
+use utf8;
+
+use POSIX;
+use DateTime;
+use XML::LibXML;
+use Spreadsheet::ParseExcel;
+
+use NonameTV qw/MyGet norm MonthNumber/;
+use NonameTV::DataStore::Helper;
+use NonameTV::Log qw/progress error/;
+use NonameTV::Config qw/ReadConfig/;
+
+use NonameTV::Importer::BaseMonthly;
+
+use base 'NonameTV::Importer::BaseMonthly';
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my $self  = $class->SUPER::new( @_ );
+  bless ($self, $class);
+
+  defined( $self->{UrlRoot} ) or die "You must specify UrlRoot";
+
+  $self->{MinMonths} = 0 unless defined $self->{MinMonths};
+  $self->{MaxMonths} = 1 unless defined $self->{MaxMonths};
+
+  my $conf = ReadConfig();
+
+  $self->{FileStore} = $conf->{FileStore};
+
+  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore} );
+  $self->{datastorehelper} = $dsh;
+
+  return $self;
+}
+
+sub ApproveContent {
+  my $self = shift;
+  my( $cref, $callbackdata ) = @_;
+
+  if( $$cref eq '' ) {
+    return "404 not found";
+  }
+  if( $$cref =~ 'File Not Found' ) {
+    return "404 not found";
+  }
+   else {
+    return undef;
+  }
+}
+
+sub Object2Url {
+  my $self = shift;
+  my( $objectname, $chd ) = @_;
+
+  my( $year, $month ) = ( $objectname =~ /(\d+)-(\d+)$/ );
+ 
+ 
+  my $dt = DateTime->new(
+      year       => $year,
+      month      => $month,
+      time_zone  => 'Europe/Stockholm',
+  );
+    
+  for( my $v=1; $v<=4; $v++ ){
+  		my $url = $self->{UrlRoot} . '/' .
+    		$chd->{grabber_info} . ' ' . $dt->month_name . ' ' . $dt->year . ' v' . $v . '.xls';
+        return( $url, undef );
+  }
+
+}
+
+sub ImportContent
+{
+  my $self = shift;
+  my( $batch_id, $cref, $chd ) = @_;
+
+  my $dsh = $self->{datastorehelper};
+  my $ds = $self->{datastore};
+
+  my %columns = ();
+  my $date;
+  my $currdate = "x";
+
+  my $oBook = Spreadsheet::ParseExcel::Workbook->Parse( $cref );
+
+  # main loop
+  for(my $iSheet=0; $iSheet < $oBook->{SheetCount} ; $iSheet++) {
+
+    my $oWkS = $oBook->{Worksheet}[$iSheet];
+    progress( "BBCWW: $chd->{xmltvid}: Processing worksheet: $oWkS->{Name}" );
+
+    # browse through rows
+    for(my $iR = $oWkS->{MinRow} ; defined $oWkS->{MaxRow} && $iR <= $oWkS->{MaxRow} ; $iR++) {
+
+
+      # get the names of the columns from the 1st row
+      # the columns that we use are
+      # Date
+      # Time
+      # Programme Title
+      # Series Number (Season)
+      # Episode Number
+      # Episode Title
+      # Synopsis
+      if( not %columns ){
+        for(my $iC = $oWkS->{MinCol} ; defined $oWkS->{MaxCol} && $iC <= $oWkS->{MaxCol} ; $iC++) {
+
+          $columns{norm($oWkS->{Cells}[$iR][$iC]->Value)} = $iC;
+        }
+        next;
+      }
+
+      # date - column 0 ('Date')
+      my $oWkC = $oWkS->{Cells}[$iR][$columns{'Date'}];
+      next if( ! $oWkC );
+      next if( ! $oWkC->Value );
+      $date = ParseDate( $oWkC->Value );
+      next if( ! $date );
+
+      if( $date ne $currdate ) {
+
+        progress("BBCWW: $chd->{xmltvid}: Date is $date");
+
+        $dsh->StartDate( $date , "00:00" ); 
+        $currdate = $date;
+      }
+
+      # starttime - column ('Time')
+      $oWkC = $oWkS->{Cells}[$iR][$columns{'Time'}];
+      next if( ! $oWkC );
+      next if( ! $oWkC->Value );
+      my $starttime = ParseTime( $oWkC->Value );
+      next if( ! $starttime );
+
+      # title
+      $oWkC = $oWkS->{Cells}[$iR][$columns{'Programme Title'}];
+      next if( ! $oWkC );
+      my $title = $oWkC->Value if( $oWkC->Value );
+
+      my $epino = $oWkS->{Cells}[$iR][$columns{'Episode No.'}]->Value if $oWkS->{Cells}[$iR][$columns{'Episode No.'}];
+      my $seano = $oWkS->{Cells}[$iR][$columns{'Series No.'}]->Value if $oWkS->{Cells}[$iR][$columns{'Series No.'}];
+
+	  my $desc = $oWkS->{Cells}[$iR][$columns{'Synopsis.'}]->Value if $oWkS->{Cells}[$iR][$columns{'Synopsis.'}];
+
+	  my $subtitle = $oWkS->{Cells}[$iR][$columns{'Episode Title'}]->Value if $oWkS->{Cells}[$iR][$columns{'Episode Title'}];
+
+      progress("BBCWW: $chd->{xmltvid}: $starttime - $title");
+
+      my $ce = {
+        channel_id => $chd->{channel_id},
+        title => $title,
+        start_time => $starttime,
+        description => $desc,
+      };
+
+	  # Subtitle
+	  $ce->{subtitle} = norm( $subtitle ) if $subtitle;
+
+      if( $epino ){
+        if( $seano ){
+          $ce->{episode} = sprintf( "%d . %d .", $seano-1, $epino-1 );
+        } else {
+          $ce->{episode} = sprintf( ". %d .", $epino-1 );
+        }
+      }
+
+      $dsh->AddProgramme( $ce );
+
+    } # next row
+  } # next worksheet
+
+
+  return 1;
+}
+
+sub ParseDate {
+  my( $text ) = @_;
+
+  $text =~ s/^\s+//;
+
+  my( $dayname, $day, $monthname, $year );
+  my $month;
+
+  if( $text =~ /^\d+-\d+-\d+$/ ) { # format '2011-07-01'
+    ( $year, $month, $day ) = ( $text =~ /^(\d+)-(\d+)-(\d+)$/ );
+    $year += 2000 if $year lt 100;
+  } elsif( $text =~ /^\d+\/\d+\/\d+$/ ) { # format '01/11/2008'
+    ( $day, $month, $year ) = ( $text =~ /^(\d+)\/(\d+)\/(\d+)$/ );
+    $year += 2000 if $year lt 100;
+  }
+
+  return sprintf( '%d-%02d-%02d', $year, $month, $day );
+}
+
+sub ParseTime {
+  my( $text ) = @_;
+
+  my( $hour , $min );
+
+  if( $text =~ /^\d+:\d+$/ ){
+    ( $hour , $min ) = ( $text =~ /^(\d+):(\d+)$/ );
+  } elsif( $text =~ /^0\.\d+$/){ # format '0.377962962962964'
+    my $daysecs = int( 86400 * $text );
+    $hour = int( $daysecs / 3600 );
+    $min =  int( ( $daysecs - ( $hour * 3600 ) ) / 60 );
+  }
+
+  return sprintf( "%02d:%02d", $hour, $min );
+}
+
+1;
+
+### Setup coding system
+## Local Variables:
+## coding: utf-8
+## End:
