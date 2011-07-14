7d808f2b9bd731f21dbadbb473fe18ab02b99af3
Joakim Nylen <joakim@tvzon.se>
2011-07-15 01:37:39 +0200

    MonthNumber for swedish lanuage

--- lib/NonameTV.pm
+++ lib/NonameTV.pm
@@ -781,6 +781,15 @@ sub MonthNumber {
     @months_6 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
     @months_7 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
     @months_8 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
+  } elsif( $lang =~ /^sv$/ ){
+    @months_1 = qw/jan feb mar apr maj jun jul aug sep okt nov dec/;
+    @months_2 = qw/januari februari mars april maj juni juli augusti september oktober november december/;
+    @months_3 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
+    @months_4 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
+    @months_5 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
+    @months_6 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
+    @months_7 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
+    @months_8 = qw/1 2 3 4 5 6 7 8 9 10 11 12/;
   }
 
   my %monthnames = ();
