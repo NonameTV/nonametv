27dcd739c57bcf17d79f923df0b7e4e7b5bc29fc
Joakim Nylen <joakim@tvzon.se>
2011-07-21 03:06:24 +0200

    Preparing for Actor and producer list.

--- lib/NonameTV/Importer/NRK.pm
+++ lib/NonameTV/Importer/NRK.pm
@@ -100,7 +100,6 @@ sub ImportContent
   			( $seas ) = ($desc =~ /Sesong\s+(\d+)./ );
 
   			
-  			
   			# Avsnitt 2
   			( $ep, $eps ) = ($desc =~ /\((\d+)\:(\d+)\)/ );
         
@@ -127,6 +126,7 @@ sub ImportContent
         $episode = sprintf( ". %d .", $ep-1 );
       }
         
+         
         my $ce = {
             start_time  => $start,
             #end_time   => $stop,
@@ -138,6 +138,18 @@ sub ImportContent
         };
         
         $ce->{episode} = $episode if $episode;
+        
+        # Producers
+        #if( my( $directors ) = ($desc =~ /^Produsert\s+av\s*(.*)/) )
+    		#{
+      	#	$ce->{directors} = parse_person_list( $directors );
+    		#}
+        
+        # Get actors
+        #if( my( $actors ) = ($desc =~ /^Med\s*(.*)/ ) )
+    		#{
+      	#	$ce->{actors} = parse_person_list( $actors );
+   			#}
         
         $dsh->AddProgramme( $ce );
     
@@ -190,6 +202,31 @@ sub createDate
     
     return "$year-$month-$date";
 
+}
+
+sub parse_person_list
+{
+  my( $str ) = @_;
+  
+  # Remove all variants of m.fl.
+  $str =~ s/\s*m[\. ]*fl\.*\b//;
+  
+  # Remove trailing '.'
+  $str =~ s/\.$//;
+
+  $str =~ s/\bog\b/,/;
+
+  my @persons = split( /\s*,\s*/, $str );
+  foreach (@persons)
+  {
+    # The character name is sometimes given . Remove it.
+    # The Cast-entry is sometimes cutoff, which means that the
+    # character name might be missing a trailing ).
+    s/\s*\(.*$//;
+    s/.*\s+-\s+//;
+  }
+
+  return join( ", ", grep( /\S/, @persons ) );
 }
 
 1;
