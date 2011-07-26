fe1863b5e8134832d48eecc4cd9b85d224026c05
Joakim Nylen <joakim@tvzon.se>
2011-07-26 02:49:06 +0200

    TVP importer now uses XML instead, with a working char coding. It also now has credits, year.

--- lib/NonameTV/Importer/TVP.pm
+++ lib/NonameTV/Importer/TVP.pm
@@ -2,28 +2,21 @@ package NonameTV::Importer::TVP;
 
 use strict;
 use warnings;
-use utf8;
-#use Unicode::String;
+use Unicode::String;
 
 =pod
 
-Import data from TVP's presservice at http://www.tvp.pl/prasa/.
-The data is downloaded in ;-separated text-files.
+Importer for TVP
+Channels: TVPolonia, TVP, TVP2, TVPKultura
 
 =cut
 
+use Encode qw/decode encode/;
+use utf8;
 
-use DateTime;
-#use Encode;
-#use Encode::Encoding;
-##use utf8::is_utf8;
-##use Roman;
-
-use NonameTV qw/AddCategory norm/;
+use NonameTV qw/AddCategory normLatin1 ParseXml norm/;
 use NonameTV::DataStore::Helper;
-use NonameTV::Log qw/progress error/;
-
-use NonameTV::Importer::BaseDaily;
+use NonameTV::Log qw/w f progress/;
 
 use base 'NonameTV::Importer::BaseDaily';
 
@@ -33,13 +26,9 @@ sub new {
     my $self  = $class->SUPER::new( @_ );
     bless ($self, $class);
 
+    $self->{datastorehelper} = NonameTV::DataStore::Helper->new( $self->{datastore} );#, 'Europe/Zurich'
 
-    defined( $self->{UrlRoot} ) or die "You must specify UrlRoot";
-
-    $self->{datastore}->{SILENCE_DUPLICATE_SKIP} = 1;
-
-    my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore} );
-    $self->{datastorehelper} = $dsh;
+    $self->{datastore}->{augment} = 0;
 
     return $self;
 }
@@ -47,205 +36,162 @@ sub new {
 sub Object2Url {
   my $self = shift;
   my( $objectname, $chd ) = @_;
-
   my( $year, $month, $day ) = ( $objectname =~ /(\d+)-(\d+)-(\d+)$/ );
- 
+
+
  	my( $folder, $endtag ) = split( /:/, $chd->{grabber_info} );
  
-  my $url = sprintf( "%s%sp%02d%02d_%s.txt",
+  my $url = sprintf( "%s%sxml/p%02d%02d_%s.xml",
                      $self->{UrlRoot}, $folder, 
                      $month, $day, $endtag );
 
-  return( $url, undef );
+  # Only one url to look at and no error
+  return ([$url], undef);
+}
+
+sub FilterContent {
+  my $self = shift;
+  my( $cref, $chd ) = @_;
+
+  #$$cref = decode( 'utf-8', $$cref );
+
+  #$$cref =~ s|\x0C$||g; # strip carriage return from the line endings
+  #$$cref = normLatin1( $$cref );
+
+  #$$cref = encode( 'utf-8', $$cref );
+  #$$cref = Unicode::String::latin1 ($$cref)->utf8 ();
+
+  return( $cref, undef);
 }
 
 sub ContentExtension {
-  return 'txt';
+  return 'xml';
 }
 
 sub FilteredExtension {
-  return 'txt';
+  return 'xml';
 }
 
 sub ImportContent {
   my $self = shift;
+  my ($batch_id, $cref, $chd) = @_;
 
-  my( $batch_id, $cref, $chd ) = @_;
-
-	#my $str = decode( "utf8", $$cref );
-	#my $str = Unicode::String::utf8 ($$cref)->utf8 ();
-
-  my $ds = $self->{datastore};
   my $dsh = $self->{datastorehelper};
+  my $ds = $self->{datastore};
 
-  # Decode the string into perl's internal format.
-  # see perldoc Encode
-
-  my( $year, $month, $day ) = ( $batch_id =~ /(\d+)-(\d+)-(\d+)$/ );
-  my $date = $year."-".$month."-".$day;
-
-  my $currdate = "x";
-  my @rows = split("\n", $$cref );
-
-  if( scalar( @rows < 2 ) )
-  {
-    error( "$batch_id: No data found" );
+  my $doc = ParseXml ($cref);
+  
+  if (not defined ($doc)) {
+    f ("$batch_id: Failed to parse.");
     return 0;
   }
 
-  my $columns = [ split( "\t", $rows[0] ) ];
-
-  	if( $date ) {
-
-        progress("TVP: $chd->{xmltvid}: Date is $date");
-
-        if( $date ne $currdate ) {
-
-          #if( $currdate ne "x" ){
-          #  $dsh->EndBatch( 1 );
-          #}
-
-         # $dsh->StartBatch( $batch_id, $chd->{channel_id} );
-          $dsh->StartDate( $date , "00:00" ); 
-          $currdate = $date;
-        }
-      }
-
-  for ( my $i = 1; $i < scalar @rows; $i++ )
-  {
-  	
-  	
-  	my $test = norm($rows[$i]);
-  	
-  	if( isShow( $test ) ) {
-  		my( $time, $title, $episode, $epi_title ) = ParseShow( norm($test) );
-  		
-  		#$title = decode( "iso-8859-1", $title );
-  		
-  		#$title = encode("utf8", $title);
-  		
-  		#Encode::from_to($title, "latin7", "iso-8859-1"); #1
-  		
-  		$title = norm($title);
-  		
-  		progress("TVP: $chd->{xmltvid}: $time - $title");
-  		
-  		
-  		my $ce = {
-        channel_id => $chd->{id},
-        start_time => $time,
-        title => $title,
-      };
-      
-      $ce->{episode} = $episode if $episode;
-      
-      $ce->{subtitle} = norm($epi_title) if $epi_title;
-      
-      $dsh->AddProgramme( $ce );
-  	}
-
-
-    #$self->extract_extra_info( $ce );
-    #$dsh->AddProgramme( $ce );
+  # The data really looks like this...
+  my $ns = $doc->find ('//ROOT');
+  if( $ns->size() == 0 ) {
+    f ("$batch_id: No data found");
+    return 0;
   }
 
-	# It would be stupid if we dont end the batch
-	# $dsh->EndBatch( 1 );
-  # Success
+  my $date = $doc->findvalue( '//ROOT/DZIEN' );
+  $date =~ s|(\d+)\.(\d+)\.(\d+)|$3-$2-$1|;
+  
+  $dsh->StartDate( $date , '00:00' );
+
+
+	my $ns2 = $doc->find ('//POZYCJA_PROGRAMOWA');
+  foreach my $programme ($ns2->get_nodelist) {
+  	# Time
+    my ($time) = ($programme->findvalue ('./GODZINA_EMISJI') =~ m|(\d+\:\d+)|);
+    if( !defined( $time ) ){
+      w( 'programme without start time!' );
+    }else{
+    	$time = ParseTime($time);
+    	
+    	
+    	# Title
+      my ($title) = norm($programme->findvalue ('./TYTUL_CYKLU'));
+      if(!$title) {
+      	$title = norm($programme->findvalue ('TYTUL'));
+      }
+
+      my ($year) = $programme->findvalue ('./ROK_PRODUKCJI');
+
+      my $ce = {
+        channel_id => $chd->{id},
+        start_time => $time,
+        title => $title
+      };
+
+			# Episode
+			my ($ep) = $programme->findvalue ('./NR_ODCINKA');
+			
+			# Use year as season if found
+  		if( ($ep) and ($year) )
+   		{
+        $ce->{episode} = sprintf( "%d . %d .", $year-1, $ep-1 );
+   		} elsif(($ep) and (!$year)) {
+   			$ce->{episode} = sprintf( ". %d .", $ep-1 );
+   		}
+			
+
+			# Stereo (It's actually in the correct form)
+			my ($stereo) = $programme->findvalue ('./DZWIEK');
+			$ce->{stereo} = $stereo if $stereo;
+
+			# Aspect (It's actually in the correct form)
+			my ($aspect) = $programme->findvalue ('./FORMAT_OBRAZU');
+			$ce->{aspect} = $aspect if $aspect;
+
+			# Actors (It's actually in the correct form)
+			my ($actors) = $programme->findvalue ('./WYKONAWCY');
+			$ce->{actors} = $actors if $actors;
+
+			# Presenters (It's actually in the correct form)
+			my ($presenters) = $programme->findvalue ('./REZYSER');
+			$ce->{presenters} = $presenters;
+
+			# Genre
+      my ($genre) = $programme->findvalue ('./RODZAJ');
+      my ( $program_type, $categ ) = $self->{datastore}->LookupCat( "TVP", $genre );
+      AddCategory( $ce, $program_type, $categ );
+      
+      # Production Year
+      if( $year ){
+        $ce->{production_date} = $year . '-01-01';
+      }
+
+			#progress("TVP: $chd->{xmltvid}: $time - $title");
+      $dsh->AddProgramme( $ce );
+    }
+  }
+
   return 1;
 }
 
-sub isShow {
-  my ( $text ) = @_;
 
-  # format '14.00 Gudstjänst med LArs Larsson - detta är texten'
-  if( $text =~ /^\d+\:\d+\s+\S+/i ){
-    return 1;
-  }
-
-  return 0;
-}
-
-sub ParseShow {
+sub ParseTime {
   my( $text ) = @_;
 
-  my( $time, $title, $desc, $ep, $eps, $episode_title, $seas );
+  my( $hour , $min );
 
-  ( $time, $title ) = ( $text =~ /^(\d+\:\d+)\s+(.*)$/ );
-
-  # parse episode id, 
-  # format '14.00 Gudstjänst med LArs Larsson - detta är texten'
-  if( $title =~ /-\s+(.*)$/ ){
-    my ( $esen ) = ( $title =~ /-\s+(.*)$/ );
-    
-    ## Parse episode
-    ( $ep, $eps ) = ($esen =~ /odc.\s+(\d+)\/(\d+)/ );
-    ( $ep ) = ($esen =~ /odc.\s+(\d+)/ );
-    
-    ## Parse season <- NOT WORKING
-    ##my ( $roman ) = ($esen =~ /seria\s+(\w+)\s+/ );
-    ##if((defined $roman) and (isroman($roman))) {
-    ##	my $seas = arabic($roman);
-		##}
-    ## Parse episode title
-    ( $episode_title ) = ($title =~ /\"(.*)\"/ );
-    $title =~ s/\"(.*)\"//;
-    
-    
-    $title =~ s/-\s+(.*)$//;
+  if( $text =~ /^\d+:\d+$/ ){
+    ( $hour , $min ) = ( $text =~ /^(\d+):(\d+)$/ );
   }
   
-  ## I don't know how to extract the description, but it is on the end of the ; (last ;)
-  if( $title =~ /;\s+(.*)$/ ){
-    ( $desc ) = ( $title =~ /;\s+(.*)$/ );
-    $title =~ s/;\s+(.*)$//;
+  # Sometimes hour is 24, then it is 00
+  if ($hour eq '24') {
+  	$hour = '00';
   }
   
-  ## Extract extra shit in the title.
-  if( $title =~ /,\s+(.*)$/ ){
-    $title =~ s/,\s+(.*)$//;
+  # Sometimes hour is 25, then it is 01
+  if ($hour eq '25') {
+  	$hour = '01';
   }
   
-  if( $title =~ /\s+\((.*)\)/ ){
-    $title =~ s/\s+\((.*)\)//;
-  }
   
-  if( $title =~ /\/(.*)\// ){
-    $title =~ s/\/(.*)\///;
-  }
- 	
- 	$title =~ s/\((.*)\)//g;
-
-
-  my ( $hour , $min ) = ( $time =~ /^(\d+):(\d+)$/ );
   
-  if($hour eq 24) {
-  	$hour = 00;
-  }
-  
-	$time = sprintf( "%02d:%02d", $hour, $min );
-
-
-			my( $episode );
-    # Episode info in xmltv-format
-      if( (defined $ep) and (defined $seas) and (defined $eps) )
-      {
-        $episode = sprintf( "%d . %d/%d .", $seas-1, $ep-1, $eps );
-      }
-      elsif( (defined $ep) and (defined $seas) and !(defined $eps) )
-      {
-        $episode = sprintf( "%d . %d .", $seas-1, $ep-1 );
-      }
-      elsif( (defined $ep) and (defined $eps) and !(defined $seas) )
-      {
-        $episode = sprintf( ". %d/%s .", $ep-1, $eps );
-      }
-      elsif( (defined $ep) and !(defined $seas) and !(defined $eps) )
-      {
-        $episode = sprintf( ". %d .", $ep-1 );
-      }
-
-  return( $time, $title, $episode, $episode_title );
+  return sprintf( "%02d:%02d", $hour, $min );
 }
 
 1;
