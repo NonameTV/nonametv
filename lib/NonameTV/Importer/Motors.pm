18d5405dccde080e8def83c075823d75590195c0
Joakim Nylen <joakim@tvzon.se>
2011-09-19 08:26:57 +0200

    Motors fix.

--- lib/NonameTV/Importer/Motors.pm
+++ lib/NonameTV/Importer/Motors.pm
@@ -124,6 +124,16 @@ sub ImportXLS {
       $oWkC = $oWkS->{Cells}[$iR][$columns{'Horaire'}];
       next if( ! $oWkC );
       my $time = $oWkC->Value if( $oWkC->Value );
+      
+      # Sometimes Motors somehow add 24:00:00 in the time field, that fucks the system up. 
+      my ( $hour , $min ) = ( $time =~ /^(\d+):(\d+)/ );
+      if($hour eq "24") {
+      	$hour = "00";
+      }
+      
+      $time = $hour.":".$min;
+
+			# End
 
       # title - column 2 ('Titre du produit')
       $oWkC = $oWkS->{Cells}[$iR][$columns{'Titre du produit'}];
