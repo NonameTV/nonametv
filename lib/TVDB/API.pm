883141cd54859ff26dc47b1c7f653d1ee793fab7
Joakim Nylen <joakim@tvzon.se>
2011-09-21 09:49:30 +0200

    dekarl fix: # bug

--- lib/TVDB/API.pm
+++ lib/TVDB/API.pm
@@ -187,6 +187,8 @@ sub _download {
 	$url = sprintf($fmt, $url, @parm);
 	&verbose(2, "TVDB::API: download: $url\n");
 	utf8::encode($url);
+	
+	$url =~ s|#|%23|g; # special case for # in parameter
 
 	# Make sure we only download once even in a session
 	return $self->{dload}->{$url} if defined $self->{dload}->{$url};
