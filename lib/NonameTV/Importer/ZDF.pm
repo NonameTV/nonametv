37a600eaddd270918d94c3ca1b187138cdabd636
Karl Dietz <dekarl@users.sourceforge.net>
2011-12-09 09:12:09 +0100

    ZDF*: limit to 6 weeks

--- lib/NonameTV/Importer/ZDF.pm
+++ lib/NonameTV/Importer/ZDF.pm
@@ -37,6 +37,10 @@ sub new {
     defined( $self->{Username} ) or die "You must specify Username";
     defined( $self->{Password} ) or die "You must specify Password";
 
+    if ($self->{MaxWeeks} > 6) {
+      $self->{MaxWeeks} = 6;
+    }
+
     $self->{datastore}->{augment} = 1;
 
     $self->{ZDFProgrammdienstStation} = '01';
