250aa100e0e19a3ca03e396879fab54e44428fa1
Joakim Nylen <joakim@tvzon.se>
2012-03-12 01:44:19 +0100

    Changes, need to get this fixed Batching aint working.

--- lib/NonameTV/Importer/NonstopWeb.pm
+++ lib/NonameTV/Importer/NonstopWeb.pm
@@ -17,9 +17,9 @@ use XML::LibXML;
 use HTTP::Date;
 use Data::Dumper;
 
-use NonameTV qw/ParseXml normUtf8 normUtf8Utf8 AddCategory/;
-use NonameTV::Log qw/w progress error f/;
+use NonameTV qw/ParseXml normUtf8 AddCategory/;
 use NonameTV::DataStore::Helper;
+use NonameTV::Log qw/w progress error f/;
 
 use NonameTV::Importer::BaseMonthly;
 
@@ -54,7 +54,7 @@ sub Object2Url {
   my $url = $self->{UrlRoot} .
     $chd->{grabber_info} . '/' . $year . '/' . $month;
 
-  return( $url, undef );
+  #return( $url, undef );
 }
 
 sub ApproveContent {
@@ -75,8 +75,12 @@ sub FilterContent {
 
   my( $chid ) = ($chd->{grabber_info} =~ /^(\d+)/);
 
+	#$$cref =~ s|encoding="utf-8"|encoding="utf8" |;
+
   my $doc;
-  $doc = ParseXml( $$cref );
+  #$doc = ParseXml( $cref );
+  my $xml = XML::LibXML->new;
+  eval { $doc = $xml->parse_string($$cref); };
 
   if( not defined $doc ) {
     return (undef, "ParseXml failed" );
@@ -108,16 +112,19 @@ sub ImportContent
 
   my( $batch_id, $cref, $chd ) = @_;
 
-  print Dumper( $chd );
-
   my $ds = $self->{datastore};
   #$ds->{SILENCE_END_START_OVERLAP}=1;
   #$ds->{SILENCE_DUPLICATE_SKIP}=1;
   my $dsh = $self->{datastorehelper};
   
+    
+  
   my $currdate = "x";
   my $xmltvid = $chd->{xmltvid};
   my $channel_id = $chd->{id};
+  print Dumper( $chd, $dsh, $batch_id , $channel_id );
+  
+  
   my $xml = XML::LibXML->new;
   my $doc;
   eval { $doc = $xml->parse_string($$cref); };
@@ -128,95 +135,162 @@ sub ImportContent
   }
   
   # Find all "Schedule"-entries.
-  my $ns = $doc->find( "//z:row" );
-
-  if( $ns->size() == 0 )
-  {
-    f "No data found 2";
-    return 0;
-  }
-  
-  #$dsh->StartBatch( $batch_id , $channel_id );
-  foreach my $sc ($ns->get_nodelist)
-  {
-    my $start = $self->create_dt( $sc->findvalue( './@SlotLocalStartTime' ) );
-    if( not defined $start )
-    {
-      w "Invalid starttime '" 
-          . $sc->findvalue( './@SlotLocalStartTime' ) . "'. Skipping.";
-      next;
+    my $ns = $doc->find( "//z:row" );
+    
+    if( $ns->size() == 0 ) {
+        error( "Nonstop: $chd->{xmltvid}: No data found" );
+        return;
     }
     
-    #print Dumper($start);
-    
-    ## Batch
-    if( $start->ymd("-") ne $currdate ){
+    my $column;
 
-        progress("Nonstop: Date is ".$start->ymd("-"));
-
-        if( $currdate ne "x" ) {
-          #$dsh->EndBatch( 1 );
+    foreach my $sc ($ns->get_nodelist) {
+        my $start = $self->create_dt( $sc->findvalue( './@SlotUTCStartTime' ) );
+        if( not defined $start )
+        {
+            w "Invalid starttime '" 
+            . $sc->findvalue( './@SlotUTCStartTime' ) . "'. Skipping.";
+            next;
         }
-
-        #my $batch_id_day = $xmltvid . "_" . $start->ymd("-");
         
-        $dsh->StartDate( $start->ymd("-")  );#, "00:00"
-        $currdate = $start->ymd("-");
-    }
-    
-    ## Title
-    my $title_original = $sc->findvalue( './@SeriesOriginalTitle' );
-    my $title_programme = $sc->findvalue( './@ProgrammeSeriesTitle' );
-    my $title = normUtf8($title_programme) || normUtf8($title_original);
-    
-    my $desc = undef;
-    my $desc_episode = $sc->findvalue( './@ProgrammeEpisodeLongSynopsis' );
-	my $desc_series  = $sc->findvalue( './@ProgrammeSeriesLongSynopsis' );
-	$desc = $desc_episode || $desc_series;
-	
-	my $genre = $sc->findvalue( './@SeriesGenreDescription' );
-	my $production_year = $sc->findvalue( './@ProgrammeSeriesYear' );
-	#my $subtitle =  $sc->findvalue( './@ProgrammeEpisodeTitle' );
+        # Date
+        my $date = $start->ymd("-");
+        my $time = $start->hms(":");
+        
+        ## Title
+        my $title_original = $sc->findvalue( './@SeriesOriginalTitle' );
+        my $title_programme = $sc->findvalue( './@ProgrammeSeriesTitle' );
+        my $title = normUtf8($title_programme) || normUtf8($title_original);
+       #my $title = normUtf8($title_original)  || normUtf8($title_programme);
+        
+        ## Batch
+        if($date ne $currdate ) {
+            if( $currdate ne "x" ) {
+                #$dsh->EndBatch( 1 );
+            }
 
-	progress("Nonstop: $chd->{xmltvid}: $start - $title");
+            my $batchid = $xmltvid . "_" . $date;
+            #$dsh->StartBatch( $batch_id , $channel_id );
+            $dsh->StartDate( $date , "06:00" );
+            $currdate = $date;
 
-    my $ce = {
-      title 	  => normUtf8($title),
-      channel_id  => $chd->{id},
-      description => normUtf8($desc),
-      start_time  => $start->hms(":"),
-    };
-    
-    #$ce->{subtitle} = $subtitle if $subtitle;
-    
-    my ( $dummy, $season, $episode ) = ($desc =~ /\(S(.*)song\s*(\d+)\s*avsnitt\s*(\d+)\)/ );
-    
-    if((defined $season) and ($episode > 0) and ($season > 0) )
-    {
-      $ce->{episode} = sprintf( "%d . %d .", $season-1, $episode-1 );
-    }
-    elsif((defined $episode) and ($episode > 0) )
-    {
-      $ce->{episode} = sprintf( ". %d .", $episode-1 );
-    }
-    
-    if( defined( $production_year ) and ($production_year =~ /(\d\d\d\d)/) )
-    {
-      $ce->{production_date} = "$1-01-01";
-    }
-    
-    if( $genre ){
-			my($program_type, $category ) = $ds->LookupCat( 'Nonstop', $genre );
-			AddCategory( $ce, $program_type, $category );
-	}
+            progress("Nonstop: Date is: $date");
+        }
+        
+        ## Description
+        my $desc = undef;
+        my $desc_episode = $sc->findvalue( './@ProgrammeEpisodeLongSynopsis' );
+        my $desc_series  = $sc->findvalue( './@ProgrammeSeriesLongSynopsis' );
+        $desc = $desc_episode || $desc_series;
+        
+        my $genre = $sc->findvalue( './@SeriesGenreDescription' );
+       #my $subgenre = $sc->findvalue( './@SeriesSubGenreDescription' ); # Same data as the one above, somehow.
+        my $production_year = $sc->findvalue( './@ProgrammeSeriesYear' );
+        
+        # Subtitle, DefaultEpisodeTitle contains the original episodetitle.
+        # I.e. Plastic Buffet for Robot Chicken
+        # For some series (mostly on TNT7) defaultepisodetitle contains (Part {episodenum})
+        # That should be remove later on, but for now you should use Tvdb augmenter for that.
+        my $subtitle_episode = $sc->findvalue( './@ProgrammeEpisodeTitle' );
+        my $subtitle_default = $sc->findvalue( './@DefaultEpisodeTitle' );
+        my $subtitle = normUtf8($subtitle_default) || normUtf8($subtitle_episode);
+        
+        
+        progress("Nonstop: $xmltvid: $time - $title");
 
-    $dsh->AddProgramme( $ce );
-  }
+        my $ce = {
+            title       => $title,
+            channel_id  => $channel_id,
+            description => normUtf8($desc),
+            start_time  => $time,
+        };
+
+        my ( $dummy, $season, $episode ) = ($desc =~ /\(S(.*)song\s*(\d+)\s*avsnitt\s*(\d+)\)/ );
+    
+        if((defined $season) and ($episode > 0) and ($season > 0) )
+        {
+            $ce->{episode} = sprintf( "%d . %d .", $season-1, $episode-1 );
+            $ce->{program_type} = "series";
+        }
+        elsif((defined $episode) and ($episode > 0) )
+        {
+            $ce->{episode} = sprintf( ". %d .", $episode-1 );
+            $ce->{program_type} = "series";
+        }
+        
+        $ce->{description} =~ s/\(S(.*)song(.*)\)$//;
+        
+        # Year (it should actually get year from augmenter instead (as sometimes it's the wrong year))
+        if( defined( $production_year ) and ($production_year =~ /(\d\d\d\d)/) )
+        {
+            $ce->{production_date} = "$1-01-01";
+        }
+        
+        
+        # Genre
+        if( $genre ){
+            my($program_type, $category ) = $ds->LookupCat( 'Nonstop', $genre );
+            AddCategory( $ce, $program_type, $category );
+        }
+        
+        # HD
+        if($sc->findvalue( './@HighDefinition' ) eq "1") {
+            $ce->{quality} = "HDTV";
+        }
+        
+        # On movies, the subtitle (defaultepisodetitle) is same as seriestitle
+        if($title ne $subtitle) {
+            #if(defined($ce->{program_type}) and ($ce->{program_type} ne "movie")) {
+                $ce->{subtitle} = $subtitle if $subtitle;
+            #}
+        }
+        
+        # Get credits
+        # Make arrays
+        my @actors;
+        my @directors;
+        my @writers;
+    
+        # Change $i if they add more actors in the future
+        for( my $v=1; $v<=5; $v++ ) {
+            my $actor_name = normUtf8($sc->findvalue( './@ProgrammeSeriesCreditsContact' . $v ));
+            my $job = $sc->findvalue( './@ProgrammeSeriesCreditsCredit' . $v );
+            # Check if it's defined (that that actor is already in the xmlfeed)
+            if(defined($actor_name)) {
+                # Check the job
+                if(defined($job) and $job =~ /Act/) {
+                    push(@actors, $actor_name);
+                }
+                if(defined($job) and $job =~ /Himself/) {
+                    push(@actors, $actor_name);
+                }
+                if(defined($job) and $job =~ /Director/) {
+                    push(@directors, $actor_name);
+                }
+                if(defined($job) and $job =~ /Creator/) {
+                    push(@writers, $actor_name);
+                }
+                
+            }
+        }
+        
+        # Get the peoples.
+        $ce->{actors} = join( ", ", grep( /\S/, @actors ) );
+        $ce->{directors} = join( ", ", grep( /\S/, @directors ) );
+        $ce->{writers} = join( ", ", grep( /\S/, @writers ) );
+        
+        # Remove big subtitle for Commerical programmes.
+        if($ce->{title} eq "Commercial programming") {
+            $ce->{subtitle} = undef;
+        }
+        
+        $dsh->AddProgramme( $ce );
+    }
+    
+    #$dsh->EndBatch( 1 );
   
-  #$dsh->EndBatch( 1 );
-  
-  # Success
-  return 1;
+    # Success
+    return 1;
 }
 
 sub create_dt
