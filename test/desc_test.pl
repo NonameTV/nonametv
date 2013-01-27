0f32e83366662a0c86954f03b438b33f2ce9a7bb
Joakim Nylén <joakim@tvzon.se>
2013-01-27 03:35:28 +0100

    Test

--- test/desc_test.pl
+++ test/desc_test.pl
@@ -4,13 +4,7 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-my $desc = "1962, 160 Mins, B/W, Dir: Stanley Kubrick, Act: James Mason,Shelley Winters,Peter Sellers,Sue Lyon, Sub: ARB, DAN, DUT, ENG, GRE, HEB, NOR, SWE";
-if( $desc =~ /Dir:/ ) {
-	print("MATCH\n");
-	my ( $dir, $actor ) = ( $desc =~ /Dir:\s+([A-Z].+?),\s+Act:\s+([A-Z].+?),\s+Sub/ );
-	my @directors = split( /\s*,\s*/, $dir );
-  my $dire = join( ", ", grep( /\S/, @directors ) );
-	my @actors = split( /\s*,\s*/, $actor );
-  my $acte = join( ", ", grep( /\S/, @actors ) );
-	print Dumper($desc, $dir, $dire, @directors, $actor, $acte, @actors);
-}
+my $text = "Filmklubben Norden: Hämnden";
+$text =~ s/^Filmklubben\s+Norden:\s*//;
+
+print("$text\n");
