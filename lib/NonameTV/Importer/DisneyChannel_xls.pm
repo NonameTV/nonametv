d81dec47be9898fe4db1c526cd82afc481262527
Joakim Nylén <joakim@tvzon.se>
2013-02-12 17:46:41 +0100

    Seasons and episodes

---
+++ lib/NonameTV/Importer/DisneyChannel_xls.pm
@@ -0,0 +1,257 @@
+package NonameTV::Importer::DisneyChannel_xls;
+
+use strict;
+use warnings;
+
+=pod
+
+Import data for Disney Channels.
+With Season and Episode data.
+
+Features:
+
+=cut
+
+use utf8;
+
+use DateTime;
+use Spreadsheet::ParseExcel;
+use Data::Dumper;
+
+use NonameTV qw/norm AddCategory MonthNumber/;
+use NonameTV::DataStore::Helper;
+use NonameTV::Log qw/progress error/;
+use NonameTV::Config qw/ReadConfig/;
+
+use NonameTV::Importer::BaseFile;
+
+use base 'NonameTV::Importer::BaseFile';
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my $self  = $class->SUPER::new( @_ );
+  bless ($self, $class);
+
+  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore} );
+  $self->{datastorehelper} = $dsh;
+
+  $self->{datastore}->{augment} = 1;
+
+  return $self;
+}
+
+sub ImportContentFile {
+  my $self = shift;
+  my( $file, $chd ) = @_;
+
+  $self->{fileerror} = 0;
+
+  if( $file =~ /\.xls$/i ){
+    $self->ImportFlatXLS( $file, $chd );
+  } else {
+    error( "Disney: Unknown file format: $file" );
+  }
+
+  return;
+}
+
+sub ImportFlatXLS
+{
+  my $self = shift;
+  my( $file, $chd ) = @_;
+
+  my $dsh = $self->{datastorehelper};
+  my $ds = $self->{datastore};
+
+  my %columns = ();
+  my $date;
+  my $currdate = "x";
+
+  progress( "Disney FlatXLS: $chd->{xmltvid}: Processing flat XLS $file" );
+
+  my $oBook = Spreadsheet::ParseExcel::Workbook->Parse( $file );
+
+  my($iR, $oWkS, $oWkC);
+  my( $time, $episode );
+  my( $program_title , $program_description );
+  my($program_type, $category );
+  my @ces;
+  
+  # main loop
+  foreach my $oWkS (@{$oBook->{Worksheet}}) {
+
+    # start from row 3
+    #for(my $iR = $oWkS->{MinRow} ; defined $oWkS->{MaxRow} && $iR <= $oWkS->{MaxRow} ; $iR++) {
+    for(my $iR = 2 ; defined $oWkS->{MaxRow} && $iR <= $oWkS->{MaxRow} ; $iR++) {
+
+      # date (column 1)
+      $oWkC = $oWkS->{Cells}[$iR][0];
+	  $date = ParseDate( $oWkC->Value );
+	  
+	  if($date ne $currdate ) {
+        if( $currdate ne "x" ) {
+			# save last day if we have it in memory
+		#	FlushDayData( $channel_xmltvid, $dsh , @ces );
+			$dsh->EndBatch( 1 );
+        }
+
+
+		#print("Date: $date\n");
+
+
+        my $batchid = $chd->{xmltvid} . "_" . $date;
+        $dsh->StartBatch( $batchid , $chd->{id} );
+        $dsh->StartDate( $date , "06:00" );
+        $currdate = $date;
+
+        progress("Disney: Date is: $date");
+      }
+	  
+	  # time (column 1)
+      $oWkC = $oWkS->{Cells}[$iR][1];
+      next if( ! $oWkC );
+      my $time = ParseTime( $oWkC->Value );
+      next if( ! $time );
+
+	  
+	  my $title;
+	  my $test;
+	  my $season;
+	  my $episode;
+	  
+      # program_title (column 4)
+      $oWkC = $oWkS->{Cells}[$iR][4];
+	  $title = norm($oWkC->Value);
+	  
+	  
+	  $oWkC = $oWkS->{Cells}[$iR][10];
+      my $desc = $oWkC->Value;
+
+      if( $time and $title ){
+	  
+	  # empty last day array
+      undef @ces;
+      
+        progress("$time - $title");
+
+        my $ce = {
+          channel_id   => $chd->{id},
+		  title		   => norm($title),
+          start_time   => $time,
+		  description  => norm($desc),
+        };
+
+		## Episode
+		$oWkC = $oWkS->{Cells}[$iR][6];
+     	my $episode = $oWkC->Value;
+     	
+     	$oWkC = $oWkS->{Cells}[$iR][5];
+     	my $season = $oWkC->Value;
+     	
+     	# genre (column 6)
+        $oWkC = $oWkS->{Cells}[$iR][7];
+	    my $genre = norm($oWkC->Value) if( $oWkC );
+      
+      	if($episode > 0) {
+      		$ce->{episode} = ". " . ($episode-1) . " ." if $episode ne "";
+		}
+		
+		if(defined($ce->{episode}) and $season > 0) {
+			$ce->{episode} = $season . $ce->{episode};
+		}
+		## END
+		
+		if( defined($genre) and $genre ne "" ){
+			my ($program_type2, $category2 ) = $ds->LookupCat( 'DisneyChannel_xls', $genre );
+			AddCategory( $ce, $program_type2, $category2 );
+		}
+		
+		# movie
+		if($episode eq 1 and $season eq 0) {
+			$ce->{episode} = undef;
+			$ce->{program_type} = "movie";
+		}
+		
+		# Find production year from description.
+	    if( $ce->{description} =~ /^\((\d\d\d\d)\)/ )
+	    {
+	    	$ce->{description} =~ s/^\((\d\d\d\d)\) //;
+	    	$ce->{production_date} = "$1-01-01";
+	    }
+		
+		#print Dumper($ce);
+		
+        $dsh->AddProgramme( $ce );
+		
+		#push( @ces , $ce );
+      }
+
+    } # next row
+	
+  } # next worksheet
+
+  $dsh->EndBatch( 1 );
+  
+  return;
+}
+
+sub ParseDate {
+  my ( $text ) = @_;
+
+#print ">$text<\n";
+
+  my( $year, $day, $month );
+
+  # format '2011-04-13'
+  if( $text =~ /^(\d+)\/(\d+)\/(\d+)$/i ){
+    ( $day, $month, $year ) = ( $text =~ /^(\d+)\/(\d+)\/(\d{4})$/i );
+
+  # format '2011-05-16'
+  } elsif( $text =~ /^\d{4}-\d{2}-\d{2}$/i ){
+    ( $year, $month, $day ) = ( $text =~ /^(\d{4})-(\d{2})-(\d{2})$/i );
+  }
+
+  $year += 2000 if $year < 100;
+
+  my $dt = DateTime->new(
+    year => $year,
+    month => $month,
+    day => $day,
+    time_zone => "Europe/Stockholm"
+      );
+
+  #$dt->set_time_zone( "UTC" );
+
+	#print($year."-".$month."-".$day." - ".$dt->ymd("-")."\n");
+
+	return $dt->ymd("-");
+#return $year."-".$month."-".$day;
+}
+
+sub ParseTime {
+  my( $text2 ) = @_;
+
+#print "ParseTime: >$text<\n";
+
+  my( $hour , $min );
+
+  if( $text2 =~ /^\d+:\d+$/ ){
+    ( $hour , $min ) = ( $text2 =~ /^(\d+):(\d+)$/ );
+  }
+  
+  if($hour >= 24) {
+  	$hour = $hour-24;
+  	
+  	#print("Hour: $hour\n");
+  }
+
+  return sprintf( "%02d:%02d", $hour, $min );
+}
+
+1;
+
+### Setup coding system
+## Local Variables:
+## coding: utf-8
+## End:
