19b3a876f2dfdcf4ee5e0218847b3fb215b64afe
Joakim Nylén <joakim@dotmedia.nu>
2013-10-02 01:33:45 +0200

    Mergebug fix

--- lib/TMDB/Movie.pm
+++ lib/TMDB/Movie.pm
@@ -8,8 +8,6 @@ use warnings FATAL => 'all';
 use Carp qw(croak carp);
 
 #######################
-<<<<<<< HEAD
-=======
 # LOAD CPAN MODULES
 #######################
 use Object::Tiny qw(id session);
@@ -17,7 +15,6 @@ use Params::Validate qw(validate_with :types);
 use Locale::Codes::Country qw(all_country_codes);
 
 #######################
->>>>>>> 02cb4241e8ca64ec24b6fc3827dac2b1d4db489e
 # LOAD DIST MODULES
 #######################
 use TMDB::Session;
@@ -26,131 +23,6 @@ use TMDB::Session;
 # PUBLIC METHODS
 #######################
 
-<<<<<<< HEAD
-## Constructor
-sub new {
-    my $class = shift;
-    my $args  = shift;
-
-    my $self = {};
-    bless $self, $class;
-    return $self->_init($args);
-} ## end sub new
-
-# Short Accessors
-sub info          { return shift->{_info}; }
-sub budget        { return shift->info->{budget}; }
-sub certification { return shift->info->{certification}; }
-sub homepage      { return shift->info->{homepage}; }
-sub id            { return shift->info->{id}; }
-sub imdb_id       { return shift->info->{imdb_id}; }
-sub name          { return shift->info->{name}; }
-sub overview      { return shift->info->{overview}; }
-sub rating        { return shift->info->{rating}; }
-sub released      { return shift->info->{released}; }
-sub runtime       { return shift->info->{runtime}; }
-sub tagline       { return shift->info->{tagline}; }
-sub trailer       { return shift->info->{trailer}; }
-sub url           { return shift->info->{url}; }
-
-## Year
-sub year {
-    my $self     = shift;
-    my $released = $self->released();
-    $released =~ s{\-\d{2}\-\d{2}$}{}x;
-    return $released;
-} ## end sub year
-
-## Posters
-sub posters {
-    my $self = shift;
-    my $size = shift || 'original';
-
-    my @posters;
-    foreach my $poster ( @{ $self->info->{posters} } ) {
-        next unless ( $poster->{image}->{size} =~ m{$size} );
-        push @posters, $poster->{image}->{url};
-    }
-    return @posters;
-} ## end sub posters
-
-## Backdrops
-sub backdrops {
-    my $self = shift;
-    my $size = shift || 'original';
-
-    my @backdrops;
-    foreach my $backdrop ( @{ $self->info->{backdrops} } ) {
-        next unless ( $backdrop->{image}->{size} =~ m{$size} );
-        push @backdrops, $backdrop->{image}->{url};
-    }
-    return @backdrops;
-} ## end sub backdrops
-
-## Cast & crew
-sub actors   { return shift->_cast('Actor'); }
-sub director { return shift->_cast('Director'); }
-sub producer { return shift->_cast('Producer'); }
-sub author   { return shift->_cast('Author'); }
-sub cast     { return shift->actors; }
-sub writer   { return shift->author; }
-
-## Genres
-sub genres {
-    my $self = shift;
-    my @genres;
-    foreach ( @{ $self->info->{genres} } ) { push @genres, $_->{name}; }
-    return @genres;
-} ## end sub genres
-
-## Studios
-sub studios {
-    my $self = shift;
-    my @studios;
-    foreach ( @{ $self->info->{studios} } ) { push @studios, $_->{name}; }
-    return @studios;
-} ## end sub studios
-
-#######################
-# PRIVATE METHODS
-#######################
-
-## Initialize
-sub _init {
-    my $self = shift;
-    my $args = shift;
-
-    $self->{_session} = $args->{session};
-
-    my $talk_args = {
-        method => 'Movie.getInfo',
-        params => $args->{id},
-    };
-
-    # Get info
-    my $results = $self->_session->talk($talk_args)
-        or croak "No Movie found. Please try searching instead";
-
-    # Store info
-    $self->{_info} = $results->[0];
-    return $self;
-} ## end sub _init
-
-## Session
-sub _session { return shift->{_session}; }
-
-## Cast
-sub _cast {
-    my $self = shift;
-    my $job  = shift;
-    my @members;
-    foreach my $cast ( @{ $self->info->{cast} } ) {
-        next unless ( $cast->{job} eq $job );
-        push @members, $cast->{name};
-    }
-    return @members;
-} ## end sub _cast
-=======
 ## ====================
 ## Constructor
 ## ====================
@@ -588,7 +460,6 @@ sub _image_urls {
   return @urls if wantarray;
   return \@urls;
 } ## end sub _image_urls
->>>>>>> 02cb4241e8ca64ec24b6fc3827dac2b1d4db489e
 
 #######################
 1;
--- lib/TMDB/Person.pm
+++ lib/TMDB/Person.pm
@@ -8,15 +8,12 @@ use warnings FATAL => 'all';
 use Carp qw(croak carp);
 
 #######################
-<<<<<<< HEAD
-=======
 # LOAD CPAN MODULES
 #######################
 use Object::Tiny qw(id session);
 use Params::Validate qw(validate_with :types);
 
 #######################
->>>>>>> 02cb4241e8ca64ec24b6fc3827dac2b1d4db489e
 # LOAD DIST MODULES
 #######################
 use TMDB::Session;
@@ -25,48 +22,6 @@ use TMDB::Session;
 # PUBLIC METHODS
 #######################
 
-<<<<<<< HEAD
-## Constructor
-sub new {
-    my $class = shift;
-    my $args  = shift;
-
-    my $self = {};
-    bless $self, $class;
-    return $self->_init($args);
-} ## end sub new
-
-# Short Accessors
-sub info       { return shift->{_info}; }
-sub name       { return shift->info->{name}; }
-sub birthplace { return shift->info->{birthplace}; }
-sub url        { return shift->info->{url}; }
-sub id         { return shift->info->{id}; }
-sub birthday   { return shift->info->{birthday}; }
-sub biography  { return shift->info->{biography}; }
-sub bio        { return shift->biography(); }
-
-# Filmography
-sub movies {
-    my $self = shift;
-    my @movies;
-    foreach ( @{ $self->info->{filmography} } ) { push @movies, $_->{name}; }
-    return @movies;
-} ## end sub movies
-
-# Posters
-sub posters {
-    my $self = shift;
-    my $size = shift || 'original';
-
-    my @posters;
-    foreach my $poster ( @{ $self->info->{profile} } ) {
-        next unless ( $poster->{image}->{size} =~ m{$size} );
-        push @posters, $poster->{image}->{url};
-    }
-    return @posters;
-} ## end sub posters
-=======
 ## ====================
 ## Constructor
 ## ====================
@@ -199,37 +154,11 @@ sub directed           { return shift->_crew_names('Director'); }
 sub produced           { return shift->_crew_names('Producer'); }
 sub executive_produced { return shift->_crew_names('Executive Producer'); }
 sub wrote { return shift->_crew_names('Author|Novel|Screenplay|Writer'); }
->>>>>>> 02cb4241e8ca64ec24b6fc3827dac2b1d4db489e
 
 #######################
 # PRIVATE METHODS
 #######################
 
-<<<<<<< HEAD
-## Initialize
-sub _init {
-    my $self = shift;
-    my $args = shift;
-
-    $self->{_session} = $args->{session};
-
-    my $talk_args = {
-        method => 'Person.getInfo',
-        params => $args->{id},
-    };
-
-    # Get info
-    my $results = $self->_session->talk($talk_args)
-        or croak "No Person found. Please try searching instead";
-
-    # Store info
-    $self->{_info} = $results->[0];
-    return $self;
-} ## end sub _init
-
-## Session
-sub _session { return shift->{_session}; }
-=======
 ## ====================
 ## CREW NAMES
 ## ====================
@@ -246,7 +175,6 @@ sub _crew_names {
   return @names if wantarray;
   return \@names;
 } ## end sub _crew_names
->>>>>>> 02cb4241e8ca64ec24b6fc3827dac2b1d4db489e
 
 #######################
 1;
