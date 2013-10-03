bea3f1e8649648bdd6a0e85a4fc96da5b6b2417a
Karl Dietz <dekarl@users.sourceforge.net>
2013-10-03 18:23:54 +0200

    DreiSat: add new url scheme

--- lib/NonameTV/Importer/DreiSat.pm
+++ lib/NonameTV/Importer/DreiSat.pm
@@ -39,6 +39,18 @@ sub new {
     return $self;
 }
 
+sub first_day_of_week
+{
+  my ($year, $week) = @_;
+
+  # Week 1 is defined as the one containing January 4:
+  DateTime
+    ->new( year => $year, month => 1, day => 4 )
+    ->add( weeks => ($week - 1) )
+    ->truncate( to => 'week' );
+} # end first_day_of_week
+
+
 sub Object2Url {
   my $self = shift;
   my( $objectname, $chd ) = @_;
@@ -49,10 +61,14 @@ sub Object2Url {
   my $url1 = sprintf( "http://pressetreff2.3sat.de/Public/Woche/3Sat_%04d%02d.XML", $year, $week);
   # Karl's (looks like some postprocessed version, basically the same)
   my $url2 = sprintf( "http://programmdienst.3sat.de/wspressefahne/Dateien/3sat_Woche%02d%02d.xml", $week, $year%100 );
+  # new scheme (old urls stopped working September 2013)
+  my $datefirst = first_day_of_week( $year, $week )->add( days => -2 )->dmy('.'); # saturday
+  my $datelast  = first_day_of_week( $year, $week )->add( days =>  4 )->dmy('.'); # friday
+  my $url3 = sprintf( "https://pressetreff.3sat.de/programm/download/?eID=programmdienst_dl&start=%s&stop=%s&format=xml&lang=True", $datefirst, $datelast );
 
-#  progress($self->{Type} . ": fetching data from $url");
+#  progress($self->{Type} . ": fetching data from $url3");
 
-  return( [$url1, $url2], undef );
+  return( [$url3, $url1, $url2], undef );
 }
 
 sub FilterContent {
