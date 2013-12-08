ef9cbe8de5888875c107d90c67714d921ca70ab6
Joakim Nylen <me@jnylen.nu>
2013-12-08 14:41:58 +0100

    I fucked up

--- lib/NonameTV/Augmenter/Tmdb3.pm
+++ lib/NonameTV/Augmenter/Tmdb3.pm
@@ -1,3 +1,5 @@
+package NonameTV::Augmenter::Tmdb3;
+
 use strict;
 use warnings;
 
@@ -17,41 +19,41 @@ use base 'NonameTV::Augmenter::Base';
 sub new {
     my $proto = shift;
     my $class = ref($proto) || $proto;
-    my $self  = $class-&gt;SUPER::new( @_ );
+    my $self  = $class->SUPER::new( @_ );
     bless ($self, $class);
 
 #    print Dumper( $self );
 
-    defined( $self-&gt;{ApiKey} )   or die "You must specify ApiKey";
-    defined( $self-&gt;{Language} ) or die "You must specify Language";
+    defined( $self->{ApiKey} )   or die "You must specify ApiKey";
+    defined( $self->{Language} ) or die "You must specify Language";
 
     # only consider Ratings with 10 or more votes by default
-    if( !defined( $self-&gt;{MinRatingCount} ) ){
-      $self-&gt;{MinRatingCount} = 10;
+    if( !defined( $self->{MinRatingCount} ) ){
+      $self->{MinRatingCount} = 10;
     }
 
     # only copy the synopsis if you trust their rights clearance enough!
-    if( !defined( $self-&gt;{OnlyAugmentFacts} ) ){
-      $self-&gt;{OnlyAugmentFacts} = 0;
+    if( !defined( $self->{OnlyAugmentFacts} ) ){
+      $self->{OnlyAugmentFacts} = 0;
     }
 
     # need config for main content cache path
     my $conf = ReadConfig( );
 
-#    my $cachefile = $conf-&gt;{ContentCachePath} . '/' . $self-&gt;{Type} . '/tvdb.db';
-#    my $bannerdir = $conf-&gt;{ContentCachePath} . '/' . $self-&gt;{Type} . '/banner';
+#    my $cachefile = $conf->{ContentCachePath} . '/' . $self->{Type} . '/tvdb.db';
+#    my $bannerdir = $conf->{ContentCachePath} . '/' . $self->{Type} . '/banner';
 
-    $self-&gt;{themoviedb} = TMDB-&gt;new(
-        apikey =&gt; $self-&gt;{ApiKey},
-        lang   =&gt; $self-&gt;{Language},
+    $self->{themoviedb} = TMDB->new(
+        apikey => $self->{ApiKey},
+        lang   => $self->{Language},
     );
 
-    $self-&gt;{search} = $self-&gt;{themoviedb}-&gt;search(
-        include_adult =&gt; 'false',  # Include adult results. 'true' or 'false'
+    $self->{search} = $self->{themoviedb}->search(
+        include_adult => 'false',  # Include adult results. 'true' or 'false'
     );
 
     # slow down to avoid rate limiting
-    $self-&gt;{Slow} = 1;
+    $self->{Slow} = 1;
 
     return $self;
 }
@@ -60,25 +62,25 @@ sub new {
 sub FillCredits( $$$$$ ) {
   my( $self, $resultref, $credit, $doc, $job )=@_;
 
-  my @nodes = $doc-&gt;findnodes( '/OpenSearchDescription/movies/movie/cast/person[@job=\'' . $job . '\']' );
+  my @nodes = $doc->findnodes( '/OpenSearchDescription/movies/movie/cast/person[@job=\'' . $job . '\']' );
   my @credits = ( );
   foreach my $node ( @nodes ) {
-    my $name = $node-&gt;findvalue( './@name' );
+    my $name = $node->findvalue( './@name' );
     if( $job eq 'Actor' ) {
-      my $role = $node-&gt;findvalue( './@character' );
+      my $role = $node->findvalue( './@character' );
       if( $role ) {
         # skip roles like '-', but allow roles like G, M, Q (The Guru, James Bond)
-        if( ( length( $role ) &gt; 1 )||( $role =~ m|^[A-Z]$| ) ){
+        if( ( length( $role ) > 1 )||( $role =~ m|^[A-Z]$| ) ){
           $name .= ' (' . $role . ')';
         } else {
-          w( 'Unlikely role \'' . $role . '\' for actor. Fix it at ' . $resultref-&gt;{url} . '/edit?active_nav_item=cast' );
+          w( 'Unlikely role \'' . $role . '\' for actor. Fix it at ' . $resultref->{url} . '/edit?active_nav_item=cast' );
         }
       }
     }
     push( @credits, $name );
   }
   if( @credits ) {
-    $resultref-&gt;{$credit} = join( ', ', @credits );
+    $resultref->{$credit} = join( ', ', @credits );
   }
 }
 
@@ -86,96 +88,96 @@ sub FillCredits( $$$$$ ) {
 sub FillHash( $$$ ) {
   my( $self, $resultref, $movieId, $ceref )=@_;
  
-  if( $self-&gt;{Slow} ) {
+  if( $self->{Slow} ) {
     sleep (1);
   }
-  my $movie = $self-&gt;{themoviedb}-&gt;movie( id =&gt; $movieId );
-#  print Dumper $movie-&gt;info;
-#  print Dumper $movie-&gt;alternative_titles;
-#  print Dumper $movie-&gt;cast;
-#  print Dumper $movie-&gt;crew;
-#  print Dumper $movie-&gt;images;
-#  print Dumper $movie-&gt;keywords;
-#  print Dumper $movie-&gt;releases;
-#  print Dumper $movie-&gt;trailers;
-#  print Dumper $movie-&gt;translations;
-#  print Dumper $movie-&gt;lists;
-#  print Dumper $movie-&gt;reviews;
-#  print Dumper $movie-&gt;changes;
+  my $movie = $self->{themoviedb}->movie( id => $movieId );
+#  print Dumper $movie->info;
+#  print Dumper $movie->alternative_titles;
+#  print Dumper $movie->cast;
+#  print Dumper $movie->crew;
+#  print Dumper $movie->images;
+#  print Dumper $movie->keywords;
+#  print Dumper $movie->releases;
+#  print Dumper $movie->trailers;
+#  print Dumper $movie->translations;
+#  print Dumper $movie->lists;
+#  print Dumper $movie->reviews;
+#  print Dumper $movie->changes;
 
   if (not defined ($movie)) {
-    w( $self-&gt;{Type} . ' failed to parse result.' );
+    w( $self->{Type} . ' failed to parse result.' );
     return;
   }
 
   # FIXME shall we use the alternative name if that's what was in the guide???
   # on one hand the augmenters are here to unify various styles on the other
   # hand matching the other guides means less surprise for the users
-  $resultref-&gt;{title} = norm( $movie-&gt;title );
-  if( defined( $movie-&gt;info ) ){
-    my $original_title = $movie-&gt;info-&gt;{original_title};
+  $resultref->{title} = norm( $movie->title );
+  if( defined( $movie->info ) ){
+    my $original_title = $movie->info->{original_title};
     if( defined( $original_title ) ){
-      $resultref-&gt;{original_title} = norm( $original_title );
+      $resultref->{original_title} = norm( $original_title );
     }else{
-      my $url = 'http://www.themoviedb.org/movie/' . $movie-&gt;{id};
+      my $url = 'http://www.themoviedb.org/movie/' . $movie->{id};
       w( "original title not on file, add it at $url." );
     }
   }
 
   # TODO shall we add the tagline as subtitle? (for german movies the tv title is often made of the movie title plus tagline)
-  $resultref-&gt;{subtitle} = undef;
+  $resultref->{subtitle} = undef;
 
-  $resultref-&gt;{program_type} = 'movie';
+  $resultref->{program_type} = 'movie';
 
-  if( defined( $movie-&gt;info ) ){
-    if( defined( $movie-&gt;info-&gt;{vote_count} ) ){
-      my $votes = $movie-&gt;info-&gt;{vote_count};
-      if( $votes &gt;= $self-&gt;{MinRatingCount} ){
+  if( defined( $movie->info ) ){
+    if( defined( $movie->info->{vote_count} ) ){
+      my $votes = $movie->info->{vote_count};
+      if( $votes >= $self->{MinRatingCount} ){
         # ratings range from 0 to 10
-        $resultref-&gt;{'star_rating'} = $movie-&gt;info-&gt;{vote_average} . ' / 10';
+        $resultref->{'star_rating'} = $movie->info->{vote_average} . ' / 10';
       }
     }
   }
   
   # MPAA - G, PG, PG-13, R, NC-17 - No rating is: NR or Unrated
-#  if(defined($doc-&gt;findvalue( '/OpenSearchDescription/movies/movie/certification' ) )) {
-#    my $rating = norm( $doc-&gt;findvalue( '/OpenSearchDescription/movies/movie/certification' ) );
+#  if(defined($doc->findvalue( '/OpenSearchDescription/movies/movie/certification' ) )) {
+#    my $rating = norm( $doc->findvalue( '/OpenSearchDescription/movies/movie/certification' ) );
 #    if( $rating ne '0' ) {
-#      $resultref-&gt;{rating} = $rating;
+#      $resultref->{rating} = $rating;
 #    }
 #  }
   
   # No description when adding? Add the description from themoviedb
-#  if((!defined ($ceref-&gt;{description}) or ($ceref-&gt;{description} eq "")) and !$self-&gt;{OnlyAugmentFacts}) {
-#    my $desc = norm( $doc-&gt;findvalue( '/OpenSearchDescription/movies/movie/overview' ) );
+#  if((!defined ($ceref->{description}) or ($ceref->{description} eq "")) and !$self->{OnlyAugmentFacts}) {
+#    my $desc = norm( $doc->findvalue( '/OpenSearchDescription/movies/movie/overview' ) );
 #    if( $desc ne 'No overview found.' ) {
-#      $resultref-&gt;{description} = $desc;
+#      $resultref->{description} = $desc;
 #    }
 #  }
 
-  if( exists( $movie-&gt;info()-&gt;{genres} ) ){
-    my @genres = @{ $movie-&gt;info()-&gt;{genres} };
+  if( exists( $movie->info()->{genres} ) ){
+    my @genres = @{ $movie->info()->{genres} };
     foreach my $node ( @genres ) {
-      my $genre_id = $node-&gt;{id};
-      my ( $type, $categ ) = $self-&gt;{datastore}-&gt;LookupCat( "Tmdb_genre", $genre_id );
+      my $genre_id = $node->{id};
+      my ( $type, $categ ) = $self->{datastore}->LookupCat( "Tmdb_genre", $genre_id );
       AddCategory( $resultref, $type, $categ );
     }
   }
 
   # TODO themoviedb does not store a year of production only the first screening, that should go to previosly-shown instead
-  # $resultref-&gt;{production_date} = $doc-&gt;findvalue( '/OpenSearchDescription/movies/movie/released' );
+  # $resultref->{production_date} = $doc->findvalue( '/OpenSearchDescription/movies/movie/released' );
 
-  $resultref-&gt;{url} = 'http://www.themoviedb.org/movie/' . $movie-&gt;{ id };
+  $resultref->{url} = 'http://www.themoviedb.org/movie/' . $movie->{ id };
 
-#  $self-&gt;FillCredits( $resultref, 'actors', $doc, 'Actor');
+#  $self->FillCredits( $resultref, 'actors', $doc, 'Actor');
 
-#  $self-&gt;FillCredits( $resultref, 'adapters', $doc, 'Actors');
-#  $self-&gt;FillCredits( $resultref, 'commentators', $doc, 'Actors');
-#  $self-&gt;FillCredits( $resultref, 'directors', $doc, 'Director');
-#  $self-&gt;FillCredits( $resultref, 'guests', $doc, 'Actors');
-#  $self-&gt;FillCredits( $resultref, 'presenters', $doc, 'Actors');
-#  $self-&gt;FillCredits( $resultref, 'producers', $doc, 'Producer');
-#  $self-&gt;FillCredits( $resultref, 'writers', $doc, 'Screenplay');
+#  $self->FillCredits( $resultref, 'adapters', $doc, 'Actors');
+#  $self->FillCredits( $resultref, 'commentators', $doc, 'Actors');
+#  $self->FillCredits( $resultref, 'directors', $doc, 'Director');
+#  $self->FillCredits( $resultref, 'guests', $doc, 'Actors');
+#  $self->FillCredits( $resultref, 'presenters', $doc, 'Actors');
+#  $self->FillCredits( $resultref, 'producers', $doc, 'Producer');
+#  $self->FillCredits( $resultref, 'writers', $doc, 'Screenplay');
 
 #  print STDERR Dumper( $apiresult );
 }
@@ -189,78 +191,78 @@ sub AugmentProgram( $$$ ){
   # result string, empty/false for success, message/true for failure
   my $result = '';
 
-  if( $ceref-&gt;{url} &amp;&amp; $ceref-&gt;{url} =~ m|^http://www\.themoviedb\.org/movie/\d+$| ) {
+  if( $ceref->{url} && $ceref->{url} =~ m|^http://www\.themoviedb\.org/movie/\d+$| ) {
     $result = "programme is already linked to themoviedb.org, ignoring";
     $resultref = undef;
-  } elsif( $ruleref-&gt;{matchby} eq 'movieid' ) {
-    $self-&gt;FillHash( $resultref, $ruleref-&gt;{remoteref}, $ceref );
-  } elsif( $ruleref-&gt;{matchby} eq 'title' ) {
+  } elsif( $ruleref->{matchby} eq 'movieid' ) {
+    $self->FillHash( $resultref, $ruleref->{remoteref}, $ceref );
+  } elsif( $ruleref->{matchby} eq 'title' ) {
     # search by title and year (if present)
 
-    my $searchTerm = $ceref-&gt;{title};
-    if( !$ceref-&gt;{production_date} &amp;&amp; !$ceref-&gt;{directors}){
+    my $searchTerm = $ceref->{title};
+    if( !$ceref->{production_date} && !$ceref->{directors}){
       return( undef,  "Year and directors unknown, not searching at themoviedb.org!" );
     }
 
     # filter characters that confuse the search api
-    # FIXME check again now that we encode umlauts &amp; co.
+    # FIXME check again now that we encode umlauts & co.
     $searchTerm =~ s|[-#\?\N{U+00BF}\(\)]||g;
 
-    if( $self-&gt;{Slow} ) {
+    if( $self->{Slow} ) {
       sleep (1);
     }
     # TODO fix upstream instead of working around here
-    my @candidates = $self-&gt;{search}-&gt;movie( $searchTerm );
+    my @candidates = $self->{search}->movie( $searchTerm );
     my @keep = ();
 
     my $numResult = @candidates;
-    if( $numResult &lt; 1 ){
+    if( $numResult < 1 ){
       return( undef,  "No matching movie found when searching for: " . $searchTerm );
     }else{
 
       # strip out all candidates without any matching director
-      if( ( @candidates &gt;= 1 ) and ( $ceref-&gt;{directors} ) ){
-        my @directors = split( /, /, $ceref-&gt;{directors} );
+      if( ( @candidates >= 1 ) and ( $ceref->{directors} ) ){
+        my @directors = split( /, /, $ceref->{directors} );
         my $match = 0;
 
         # loop over all remaining movies
         while( @candidates ) {
           my $candidate = shift( @candidates );
 
-          if( defined( $candidate-&gt;{id} ) ) {
+          if( defined( $candidate->{id} ) ) {
             # we have to fetch the remaining candidates to peek at the directors
-            my $movieId = $candidate-&gt;{id};
-            if( $self-&gt;{Slow} ) {
+            my $movieId = $candidate->{id};
+            if( $self->{Slow} ) {
               sleep (1);
             }
-            my $movie = $self-&gt;{themoviedb}-&gt;movie( id =&gt; $movieId );
+            my $movie = $self->{themoviedb}->movie( id => $movieId );
 
             my @names = ( );
-            foreach my $crew ( $movie-&gt;crew ) {
-              if( $crew-&gt;{'job'} eq 'Director' ) {
-                my $person = $self-&gt;{themoviedb}-&gt;person( id =&gt; $crew-&gt;{id} );
+            foreach my $crew ( $movie->crew ) {
+              if( $crew->{'job'} eq 'Director' ) {
+                my $person = $self->{themoviedb}->person( id => $crew->{id} );
                 if( defined( $person ) ){
-                  if( defined( $person-&gt;aka() ) ){
-                    if( defined( $person-&gt;aka()-&gt;[0] ) ){
+                  if( defined( $person->aka() ) ){
+                    if( defined( $person->aka()->[0] ) ){
                       # FIXME actually aka() should simply return an array
-                      my $aliases = $person-&gt;aka()-&gt;[0];
+                      my $aliases = $person->aka()->[0];
                       if( defined( $aliases ) ){
                         @names =  ( @names, @{ $aliases } );
                       }else{
-                        my $url = 'http://www.themoviedb.org/person/' . $crew-&gt;{id};
+                        my $url = 'http://www.themoviedb.org/person/' . $crew->{id};
                         w( "something is fishy with this persons aliases, see $url." );
                       }
-                      push( @names, $person-&gt;name );
+                      push( @names, $person->name );
                     }else{
-                      my $url = 'http://www.themoviedb.org/person/' . $crew-&gt;{id};
+                      my $url = 'http://www.themoviedb.org/person/' . $crew->{id};
                     w( "got a person but could not get the aliases (with [0]), see $url." );
                     }
                   }else{
-                    my $url = 'http://www.themoviedb.org/person/' . $crew-&gt;{id};
+                    my $url = 'http://www.themoviedb.org/person/' . $crew->{id};
                     w( "got a person but could not get the aliases, see $url." );
                   }
                 }else{
-                  my $url = 'http://www.themoviedb.org/person/' . $crew-&gt;{id};
+                  my $url = 'http://www.themoviedb.org/person/' . $crew->{id};
                   w( "got a reference to a person but could not get the person, see $url." );
                 }
               }
@@ -268,7 +270,7 @@ sub AugmentProgram( $$$ ){
 
             my $matches = 0;
             if( @names == 0 ){
-              my $url = 'http://www.themoviedb.org/movie/' . $candidate-&gt;{ id };
+              my $url = 'http://www.themoviedb.org/movie/' . $candidate->{ id };
               w( "director not on record, removing candidate. Add it at $url." );
             } else {
               foreach my $a ( @directors ) {
@@ -280,7 +282,7 @@ sub AugmentProgram( $$$ ){
               }
             }
             if( $matches == 0 ){
-              d( "director '" . $ceref-&gt;{directors} ."' not found, removing candidate" );
+              d( "director '" . $ceref->{directors} ."' not found, removing candidate" );
             } else {
               push( @keep, $candidate );
             }
@@ -294,17 +296,17 @@ sub AugmentProgram( $$$ ){
       }
 
       # filter out movies more then 2 years before/after if we know the year
-      if( $ceref-&gt;{production_date} ) {
-        my( $produced )=( $ceref-&gt;{production_date} =~ m|^(\d{4})\-\d+\-\d+$| );
+      if( $ceref->{production_date} ) {
+        my( $produced )=( $ceref->{production_date} =~ m|^(\d{4})\-\d+\-\d+$| );
         while( @candidates ) {
           my $candidate = shift( @candidates );
           # verify that production and release year are close
-          my $released = $candidate-&gt;{ release_date };
+          my $released = $candidate->{ release_date };
           $released =~ s|^(\d{4})\-\d+\-\d+$|$1|;
           if( !$released ){
-            my $url = 'http://www.themoviedb.org/movie/' . $candidate-&gt;{ id };
+            my $url = 'http://www.themoviedb.org/movie/' . $candidate->{ id };
             w( "year of release not on record, removing candidate. Add it at $url." );
-          } elsif( abs( $released - $produced ) &gt; 2 ){
+          } elsif( abs( $released - $produced ) > 2 ){
             d( "year of production '$produced' to far away from year of release '$released', removing candidate" );
           } else {
             push( @keep, $candidate );
@@ -316,17 +318,17 @@ sub AugmentProgram( $$$ ){
       }
 
       if( @candidates == 0 ){
-        w( 'search for "' . $ceref-&gt;{title} . '" did not return any good hit, ignoring' );
-      } elsif ( @candidates &gt; 1 ){
-        w( 'search for "' . $ceref-&gt;{title} . '" did not return a single best hit, ignoring' );
+        w( 'search for "' . $ceref->{title} . '" did not return any good hit, ignoring' );
+      } elsif ( @candidates > 1 ){
+        w( 'search for "' . $ceref->{title} . '" did not return a single best hit, ignoring' );
       } else {
-        my $movieId = $candidates[0]-&gt;{id};
+        my $movieId = $candidates[0]->{id};
 
-        $self-&gt;FillHash( $resultref, $movieId, $ceref );
+        $self->FillHash( $resultref, $movieId, $ceref );
       }
     }
   }else{
-    $result = "don't know how to match by '" . $ruleref-&gt;{matchby} . "'";
+    $result = "don't know how to match by '" . $ruleref->{matchby} . "'";
     $resultref = undef;
   }
 
