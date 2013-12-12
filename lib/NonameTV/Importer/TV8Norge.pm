f03d8cd4e7080f29e7fc3f4be49fd96e177e3205
Joakim Nylen <me@jnylen.nu>
2013-12-12 15:11:34 +0100

    New importer, TV8 Norway

---
+++ lib/NonameTV/Importer/TV8Norge.pm
@@ -0,0 +1,229 @@
+package NonameTV::Importer::TV8Norge;
+
+use strict;
+use warnings;
+
+=pod
+
+Import data for TV8 Norge.
+
+=cut
+
+use utf8;
+
+use DateTime;
+use Spreadsheet::ParseExcel;
+use Data::Dumper;
+
+use NonameTV qw/norm AddCategory MonthNumber/;
+use NonameTV::DataStore::Helper;
+use NonameTV::Log qw/progress error/;
+use NonameTV::Config qw/ReadConfig/;
+
+use NonameTV::Importer::BaseFile;
+
+use base 'NonameTV::Importer::BaseFile';
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my $self  = $class->SUPER::new( @_ );
+  bless ($self, $class);
+
+  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore} );
+  $self->{datastorehelper} = $dsh;
+
+  $self->{datastore}->{augment} = 1;
+
+  return $self;
+}
+
+sub ImportContentFile {
+  my $self = shift;
+  my( $file, $chd ) = @_;
+
+  $self->{fileerror} = 0;
+
+  if( $file =~ /\.xls$/i ){
+    $self->ImportFlatXLS( $file, $chd );
+  } else {
+    #error( "TV8Norge: Unknown file format: $file" );
+  }
+
+  return;
+}
+
+sub ImportFlatXLS
+{
+  my $self = shift;
+  my( $file, $chd ) = @_;
+
+  my $dsh = $self->{datastorehelper};
+  my $ds = $self->{datastore};
+
+  my %columns = ();
+  my $date;
+  my $currdate = "x";
+
+  #progress( "TV8Norge FlatXLS: $chd->{xmltvid}: Processing flat XLS $file" );
+
+  my $oBook = Spreadsheet::ParseExcel::Workbook->Parse( $file );
+
+    my($iR, $oWkS, $oWkC);
+
+	  my( $time, $episode );
+  my( $program_title , $program_description );
+    my @ces;
+
+  # main loop
+  foreach my $oWkS (@{$oBook->{Worksheet}}) {
+
+    for(my $iR = 1 ; defined $oWkS->{MaxRow} && $iR <= $oWkS->{MaxRow} ; $iR++) {
+
+      # date (column 1)
+      $oWkC = $oWkS->{Cells}[$iR][0];
+      next if( ! $oWkC );
+		$date = ParseDate( $oWkC->Value );
+		#$date = $oWkC->Value;
+      next if( ! $date );
+
+	  unless( $date ) {
+		progress("SKIPPING :D");
+	  next;
+	  }
+
+	  if($date ne $currdate ) {
+        if( $currdate ne "x" ) {
+			# save last day if we have it in memory
+		#	FlushDayData( $channel_xmltvid, $dsh , @ces );
+			$dsh->EndBatch( 1 );
+        }
+
+
+
+        my $batchid = $chd->{xmltvid} . "_" . $date;
+        $dsh->StartBatch( $batchid , $chd->{id} );
+        $dsh->StartDate( $date , "06:00" );
+        $currdate = $date;
+
+        progress("TV8Norge: Date is: $date");
+      }
+
+	  	#if($iR == 28) { next; }
+
+	# time (column 1)
+	 #  print "hejhejhej";
+      $oWkC = $oWkS->{Cells}[$iR][1];
+      next if( ! $oWkC );
+      my $time = ParseTime( $oWkC->Value );
+      next if( ! $time );
+
+	  #use Data::Dumper; print Dumper($oWkS->{Cells}[28]);
+
+
+	  my $title;
+	  my $test;
+	  my $season;
+	  my $episode;
+
+	  # print "hejhej";
+      # program_title (column 2)
+      $oWkC = $oWkS->{Cells}[$iR][2];
+
+      # Here's where the magic happends.
+	  # Love goes out to DrForr.
+	  $test = $oWkC->Value;
+
+	  $title = norm($test) if $test ne "";
+	  # If no series title, get it from episode name.
+
+
+	  $oWkC = $oWkS->{Cells}[$iR][6];
+      my $desc = $oWkC->Value;
+
+      if( $time and $title ){
+
+	  # empty last day array
+      undef @ces;
+
+      # Clean up the title
+      $title =~ s/\- Ny sending$//;
+      $title = norm($title);
+
+
+
+        progress("$time - $title");
+
+        my $ce = {
+          channel_id   => $chd->{id},
+		  title		   => norm($title),
+          start_time   => $date ." ". $time,
+		  description  => norm($desc),
+        };
+
+        $ds->AddProgramme( $ce );
+
+		push( @ces , $ce );
+      }
+
+    } # next row
+
+  } # next worksheet
+
+  $dsh->EndBatch( 1 );
+
+  return;
+}
+
+sub ParseDate {
+  my ( $text ) = @_;
+
+#print ">$text<\n";
+
+  my( $year, $day, $month );
+
+  # format '2011-04-13'
+  if( $text =~ /^(\d+)\/(\d+)\/(\d+)$/i ){
+    ( $day, $month, $year ) = ( $text =~ /^(\d+)\/(\d+)\/(\d{4})$/i );
+
+  # format '2011-05-16'
+  } elsif( $text =~ /^\d{4}-\d{2}-\d{2}$/i ){
+    ( $year, $month, $day ) = ( $text =~ /^(\d{4})-(\d{2})-(\d{2})$/i );
+  }
+
+  #$year += 2000 if $year < 100;
+
+  my $dt = DateTime->new(
+    year => $year,
+    month => $month,
+    day => $day,
+    time_zone => "Europe/Stockholm"
+      );
+
+  $dt->set_time_zone( "UTC" );
+
+
+	return $dt->ymd("-");
+#return $year."-".$month."-".$day;
+}
+
+sub ParseTime {
+  my( $text ) = @_;
+
+#print "ParseTime: >$text<\n";
+
+  my( $hour , $min );
+
+  if( $text =~ /^\d+:\d+$/ ){
+    ( $hour , $min ) = ( $text =~ /^(\d+):(\d+)$/ );
+  }
+
+  return sprintf( "%02d:%02d", $hour, $min );
+}
+
+1;
+
+### Setup coding system
+## Local Variables:
+## coding: utf-8
+## End:
