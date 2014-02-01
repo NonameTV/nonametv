e54d5e9cbd18aceddc6d674d7e8894e82a6fa9f8
Joakim Nylen <me@jnylen.nu>
2014-02-01 17:09:04 +0100

    New mezzo importer (new format)

--- lib/NonameTV/Importer/MezzoEN.pm
+++ lib/NonameTV/Importer/MezzoEN.pm
@@ -3,24 +3,35 @@ package NonameTV::Importer::MezzoEN;
 use strict;
 use warnings;
 
+
 =pod
 
-Importer for data from Mezzo Classic music channel. 
-One file per month downloaded from LNI site.
-The downloaded file is in xls format.
+Import data from XLSX files delivered via e-mail.
 
 Features:
 
 =cut
 
-use POSIX qw/strftime/;
+use utf8;
+
 use DateTime;
 use Spreadsheet::ParseExcel;
+use Spreadsheet::Read;
 
-use NonameTV qw/MyGet norm AddCategory/;
+use Spreadsheet::XLSX;
+use Spreadsheet::XLSX::Utility2007 qw(ExcelFmt ExcelLocaltime LocaltimeExcel);
+use Spreadsheet::Read;
+
+use Text::Iconv;
+my $converter = Text::Iconv -> new ("utf-8", "windows-1251");
+
+
+use Data::Dumper;
+use File::Temp qw/tempfile/;
+
+use NonameTV qw/norm normUtf8 AddCategory MonthNumber/;
 use NonameTV::DataStore::Helper;
 use NonameTV::Log qw/progress error/;
-use NonameTV::Config qw/ReadConfig/;
 
 use NonameTV::Importer::BaseFile;
 
@@ -32,21 +43,16 @@ sub new {
   my $self  = $class->SUPER::new( @_ );
   bless ($self, $class);
 
-  $self->{MinMonths} = 1 unless defined $self->{MinMonths};
-  $self->{MaxMonths} = 12 unless defined $self->{MaxMonths};
-
-  my $conf = ReadConfig();
-
-  $self->{FileStore} = $conf->{FileStore};
 
   my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore} );
   $self->{datastorehelper} = $dsh;
 
+  #$self->{datastore}->{augment} = 1;
+
   return $self;
 }
 
-sub ImportContentFile
-{
+sub ImportContentFile {
   my $self = shift;
   my( $file, $chd ) = @_;
 
@@ -57,234 +63,153 @@ sub ImportContentFile
   my $dsh = $self->{datastorehelper};
   my $ds = $self->{datastore};
 
-  if( $file =~ /\.xls$/i ){
-    $self->ImportXLS( $file, $channel_id, $channel_xmltvid );
+  if( $file =~ /\.xlsx$/i ){
+    $self->ImportXLS( $file, $chd );
   }
+
 
   return;
 }
 
-sub ImportXLS
-{
-  my $self = shift;
-  my( $file, $channel_id, $channel_xmltvid ) = @_;
+sub ImportXML {
+	my $self = shift;
+  my( $file, $chd ) = @_;
+  my $dsh = $self->{datastorehelper};
+  my $ds = $self->{datastore};
+  $self->{fileerror} = 1;
 
+	# Do something beautiful here later on.
+
+	error("From now on you need to convert XML files to XLS files.");
+
+	return 0;
+}
+
+sub ImportXLS {
+  my $self = shift;
+  my( $file, $chd ) = @_;
+
+  $self->{fileerror} = 0;
+
+  my $xmltvid = $chd->{xmltvid};
+  my $channel_id = $chd->{id};
   my $dsh = $self->{datastorehelper};
   my $ds = $self->{datastore};
 
-  my %columns = ();
+  # Only process .xls or .xlsx files.
+  progress( "Ginx: $xmltvid: Processing $file" );
+
+	my %columns = ();
   my $date;
   my $currdate = "x";
+  my $coldate = 0;
+  my $coltime = 1;
+  my $coltitle = 3;
+  my $colyear = 4;
+  my $coldesc = 7;
 
-  progress( "Mezzo: $channel_xmltvid: Processing XLS $file" );
+my $oBook;
 
-#return if ( $file !~ /grille_en2/ );
+if ( $file =~ /\.xlsx$/i ){ progress( "using .xlsx" );  $oBook = Spreadsheet::XLSX -> new ($file, $converter); }
+else { $oBook = Spreadsheet::ParseExcel::Workbook->Parse( $file );  }
 
-  my( $oBook, $oWkS, $oWkC );
-  $oBook = Spreadsheet::ParseExcel::Workbook->Parse( $file );
+#my $ref = ReadData ($file);
 
-  if( not defined( $oBook ) ) {
-    error( "Mezzo: $file: Failed to parse xls" );
-    return;
-  }
-
-  if( not $oBook->{SheetCount} ){
-    error( "Mezzo: $file: No worksheets found in file" );
-    return;
-  }
-
+  # main loop
   for(my $iSheet=0; $iSheet < $oBook->{SheetCount} ; $iSheet++) {
 
-    $oWkS = $oBook->{Worksheet}[$iSheet];
-    progress("Mezzo: $channel_xmltvid: Processing worksheet named '$oWkS->{Name}'");
+    my $oWkS = $oBook->{Worksheet}[$iSheet];
+    if( $oWkS->{Name} !~ /1/ ){
+      progress( "MezzoEN: Skipping other sheet: $oWkS->{Name}" );
+      next;
+    }
 
-    # read the rows with data
+    progress( "MezzoEN: Processing worksheet: $oWkS->{Name}" );
+
+	my $foundcolumns = 0;
+    # browse through rows
+    my $i = 0;
     for(my $iR = $oWkS->{MinRow} ; defined $oWkS->{MaxRow} && $iR <= $oWkS->{MaxRow} ; $iR++) {
+    $i++;
 
-      if( not %columns ){
-        # the column names are stored in the first row
-        # so read them and store their column positions
-        # for further findvalue() calls
+      my $oWkC;
 
-        for(my $iC = $oWkS->{MinCol} ; defined $oWkS->{MaxCol} && $iC <= $oWkS->{MaxCol} ; $iC++) {
+      # date
+            $oWkC = $oWkS->{Cells}[$iR][$coldate];
+            next if( ! $oWkC );
 
-          if( $oWkS->{Cells}[$iR][$iC] ){
-            $columns{$oWkS->{Cells}[$iR][$iC]->Value} = $iC;
+      	  $date = $oWkC->{Val} if( $oWkC->Value );
+            $date = ParseDate( ExcelFmt('yyyy-mm-dd', $date) );
+            next if( ! $date );
 
-            # other possible names of the columns
-            $columns{'DATE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^DATES$/ );
-            $columns{'TIME'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^TIMES$/ );
-            $columns{'TIME'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^HOURS$/ );
-            $columns{'TIME'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^HEURE$/ );
-            $columns{'TIME'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^HEURES$/ );
-            $columns{'LENGTH'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^LENGHTS$/ );
-            $columns{'LENGTH'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^DUREE$/ );
-            $columns{'LENGTH'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^DUREES$/ );
-            $columns{'TITLE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^TITLES$/ );
-            $columns{'TITLE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^TITRE$/ );
-            $columns{'TITLE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^TITRES$/ );
-            $columns{'DESCRIPTION'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^DESCRIPTIONS$/ );
-            $columns{'DESCRIPTION'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^SYNOPSIS$/ );
-            $columns{'YEAR'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^ANNEES$/ );
-            $columns{'GENRE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^GENRES$/ );
-            $columns{'DIRECTOR'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^DIRECTORS$/ );
-            $columns{'DIRECTOR'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^REALISATEURS$/ );
+      if( $date ne $currdate ){
 
-            $columns{'YEAR'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Année de production$/ );
-            $columns{'YEAR'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^AnnÈe de production$/ );
-            $columns{'DATE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Date de programmation$/ );
-            $columns{'DATE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Date de Programmation$/ );
-            $columns{'DURATION'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Durée de Prog\. \(hh:mm:ss\)$/ );
-            $columns{'DURATION'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^DurÈe de Prog\. \(hh:mm:ss\)$/ );
-            $columns{'DURATION'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Durée réelle$/ );
-            $columns{'GENRE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Genre$/ );
-            $columns{'TIME'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Heure de d/ );
-            $columns{'TIME'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Heure de début$/ );
-            $columns{'TIME'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Heure de début arrondie$/ );
-            $columns{'TIME'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Heure de dÈbut arrondie$/ );
-            $columns{'DIRECTOR'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Réalisateurs$/ );
-            $columns{'DIRECTOR'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^RÈalisateurs$/ );
-            $columns{'DESCRIPTION'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Résumé long$/ );
-            $columns{'DESCRIPTION'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^RÈsumÈ long$/ );
-            $columns{'TITLE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Titre$/ );
-            $columns{'TITLE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Titre presse$/ );
-            $columns{'TITLE'} = $iC if( $oWkS->{Cells}[$iR][$iC]->Value =~ /^Titre presse complet$/ );
-          }
-
-foreach my $cl (%columns) {
-print ">$cl<\n";
-}
-          next;
-        }
-      }
-
-      # Date
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'DATE'}];
-      next if( ! $oWkC );
-      next if( ! $oWkC->Value );
-      $date = ParseDate( $oWkC->Value );
-      next if( ! $date );
-
-      if( $date ne $currdate ) {
+        progress("MezzoEN: Date is $date");
 
         if( $currdate ne "x" ) {
           $dsh->EndBatch( 1 );
         }
 
-        my $batch_id = $channel_xmltvid . "_" . $date;
+        my $batch_id = $xmltvid . "_" . $date;
         $dsh->StartBatch( $batch_id , $channel_id );
         $dsh->StartDate( $date , "00:00" );
         $currdate = $date;
-
-        progress("Mezzo: $channel_xmltvid: Date is: $date");
       }
-      
-      # Time
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'TIME'}];
+
+      # time
+      $oWkC = $oWkS->{Cells}[$iR][$coltime];
       next if( ! $oWkC );
-      next if( ! $oWkC->Value );
 
-      my $time = ParseTime( $oWkC->Value );
-      if( not defined( $time ) ) {
-        error( "Invalid start-time '$date' '" . $oWkC->Value . "'. Skipping." );
-        next;
-      }
 
-      # Title
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'TITLE'}];
+
+      my $time = 0;  # fix for  12:00AM
+      $time=$oWkC->{Val} if( $oWkC->Value );
+
+	  #Convert Excel Time -> localtime
+      $time = ExcelFmt('hh:mm', $time);
+      $time =~ s/_/:/g; # They fail sometimes
+
+
+      # title
+      $oWkC = $oWkS->{Cells}[$iR][$coltitle];
       next if( ! $oWkC );
-      next if( ! $oWkC->Value );
-      my $title = $oWkC->Value;
-      next if( ! $title );
+      my $title = $oWkC->Value if( $oWkC->Value );
 
-      # Length
-      #$oWkC = $oWkS->{Cells}[$iR][$columns{'LENGTH'}];
-      #next if( ! $oWkC );
-      #next if( ! $oWkC->Value );
-      #my $duration = $oWkC->Value;
+      $oWkC = $oWkS->{Cells}[$iR][$coldesc];
+      my $desc = $oWkC->Value if( $oWkC );
 
-      # Description
-      my $description;
-      if( $columns{'DESCRIPTION'} ){
-        $oWkC = $oWkS->{Cells}[$iR][$columns{'DESCRIPTION'}];
-        $description = $oWkC->Value if ( $oWkC and $oWkC->Value );
-      }
-
-      # Year
-      my $year;
-      if( $columns{'YEAR'} ){
-        $oWkC = $oWkS->{Cells}[$iR][$columns{'YEAR'}];
-        $year = $oWkC->Value if ( $oWkC and $oWkC->Value );
-      }
-
-      # Genre
-      my $genre;
-      if( $columns{'GENRE'} ){
-        $oWkC = $oWkS->{Cells}[$iR][$columns{'GENRE'}];
-        $genre = $oWkC->Value if ( $oWkC and $oWkC->Value );
-      }
-
-      # Director
-      my $directors;
-      if( $columns{'DIRECTOR'} ){
-        $oWkC = $oWkS->{Cells}[$iR][$columns{'DIRECTOR'}];
-        $directors = $oWkC->Value if ( $oWkC and $oWkC->Value );
-      }
-
-      progress( "Mezzo: $channel_xmltvid: $time - $title" );
 
       my $ce = {
-        channel_id => $channel_id,
-        title => $title,
-        start_time => $time,
+        channel_id  => $channel_id,
+        start_time  => $time,
+        title 		=> norm($title),
+        description => norm($desc),
       };
 
-      $ce->{description} = $description if $description;
-      $ce->{directors} = $directors if $directors;
+      # Prod year
+	  $oWkC = $oWkS->{Cells}[$iR][$colyear];
+	  my $year = $oWkC->Value if( $oWkC );
 
-      if( $year and ( $year =~ /(\d\d\d\d)/ ) ){
-        $ce->{production_date} = "$1-01-01";
-      }
+	  if(($year) and $year ne "" and $year =~ /(\d\d\d\d)/) {
+	  	$ce->{production_date} = "$1-01-01";
+	  }
 
-      if( $genre and length( $genre ) ){
-        my($program_type, $category ) = $ds->LookupCat( "Mezzo", $genre );
-        AddCategory( $ce, $program_type, $category );
-      }
-      
-      my ( $t, $st ) = ($ce->{title} =~ /(.*)\: (.*)/);
-      if( defined( $st ) )
-      {
-	      # This program is part of a series and it has a colon in the title.
-	      # Assume that the colon separates the title from the subtitle.
-	      $ce->{title} = $t;
-	      $ce->{subtitle} = $st;
-      }
-      
-      my ( $t2, $st2 ) = ($ce->{title} =~ /(.*)\. (.*)/);
-      if( defined( $st2 ) )
-      {
-	      # This program is part of a series and it has a colon in the title.
-	      # Assume that the colon separates the title from the subtitle.
-	      $ce->{title} = $t2;
-	      $ce->{subtitle} = $st2;
-      }
-      
-      my ( $t3, $st3 ) = ($ce->{title} =~ /(.*) \- (.*)/);
-      if( defined( $st3 ) )
-      {
-	      # This program is part of a series and it has a colon in the title.
-	      # Assume that the colon separates the title from the subtitle.
-	      $ce->{title} = $t3;
-	      $ce->{subtitle} = $st3;
-      }
 
-      $dsh->AddProgramme( $ce );
+		 my( $t, $st ) = ($ce->{title} =~ /(.*)\: (.*)/);
+         if( defined( $st ) )
+         {
+              # This program is part of a series and it has a colon in the title.
+              # Assume that the colon separates the title from the subtitle.
+              $ce->{title} = $t;
+              $title = $t;
+              $ce->{subtitle} = $st;
+         }
 
-    } # next row
+	  progress("MezzoEN: $time - $title") if $title;
+      $dsh->AddProgramme( $ce ) if $title;
+    }
 
-    %columns = ();
-
-  } # next sheet
+  }
 
   $dsh->EndBatch( 1 );
 
@@ -293,42 +218,49 @@ print ">$cl<\n";
 
 sub ParseDate
 {
-  my( $dateinfo ) = @_;
+  my ( $dinfo ) = @_;
 
-#print ">$dateinfo<\n";
+  my( $day, $month, $year );
 
-  my( $month, $day, $year );
+#print ">$dinfo<\n";
 
-  if( $dateinfo =~ /^\d{4}-\d{2}-\d{2}$/ ){
-    ( $year, $month, $day ) = ( $dateinfo =~ /^(\d+)-(\d+)-(\d+)$/ );
-  } elsif( $dateinfo =~ /^\d+-\d+-\d+$/ ){
-    ( $month, $day, $year ) = ( $dateinfo =~ /^(\d+)-(\d+)-(\d+)$/ );
-  } else {
+  # format '033 03 Jul 2008'
+  if( $dinfo =~ /^\d{4}-\d{2}-\d{2}$/ ){ # format   '2010-04-22'
+    ( $year, $month, $day ) = ( $dinfo =~ /^(\d+)-(\d+)-(\d+)$/ );
+  } elsif( $dinfo =~ /^\d{2}.\d{2}.\d{4}$/ ){ # format '11/18/2011'
+    ( $month, $day, $year ) = ( $dinfo =~ /^(\d+).(\d+).(\d+)$/ );
+  } elsif( $dinfo =~ /^\d{1,2}-\d{1,2}-\d{2}$/ ){ # format '10-18-11' or '1-9-11'
+    ( $month, $day, $year ) = ( $dinfo =~ /^(\d+)-(\d+)-(\d+)$/ );
+  } elsif( $dinfo =~ /^\d{1,2}\/\d{1,2}\/\d{2}$/ ){ # format '10-18-11' or '1-9-11'
+    ( $month, $day, $year ) = ( $dinfo =~ /^(\d+)\/(\d+)\/(\d+)$/ );
+  }
+
+  else {
     return undef;
   }
 
-  $year += 2000 if( $year < 100);
+  return undef if( ! $year);
 
-  return sprintf( "%04d-%02d-%02d", $year, $month, $day );
+  $year+= 2000 if $year< 100;
+
+  my $dt = DateTime->new( year   => $year,
+                          month  => $month,
+                          day    => $day,
+                          hour   => 0,
+                          minute => 0,
+                          second => 0,
+                          );
+
+  $dt->set_time_zone( "UTC" );
+
+  return $dt->ymd();
 }
 
-sub ParseTime
-{
-  my( $timeinfo ) = @_;
 
-#print ">$timeinfo<\n";
-
-  my( $hour, $min, $sec );
-
-  if( $timeinfo =~ /^\d+:\d+:\d+$/ ){ # format '11:45:00'
-    ( $hour, $min, $sec ) = ( $timeinfo =~ /^(\d+):(\d+):(\d+)$/ );
-  } elsif( $timeinfo =~ /^\d+:\d+\s+AM\/PM$/ ){ # format '13:15 AM/PM'
-    ( $hour, $min ) = ( $timeinfo =~ /^(\d+):(\d+)\s+AM\/PM$/ );
-  } else {
-    return undef;
-  }
-
-  return sprintf( "%02d:%02d", $hour, $min );
-}
 
 1;
+
+### Setup coding system
+## Local Variables:
+## coding: utf-8
+## End:
